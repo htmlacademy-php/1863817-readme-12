17831beeaf5fe1262b2839ff919d7ecd2ef88491
Makary <pereymak2012galina@yandex.ru>
2022-04-06 21:12:05 +0300

    ++

--- schema.sql
+++ schema.sql
@@ -1,20 +1,18 @@
 CREATE DATABASE IF NOT EXISTS readme;
-DEFAULT CHARACTER SET utf8;
-DEFAULT COLLATE utf8_general_ci;
 USE readme;
 
 CREATE TABLE IF NOT EXISTS users (
-  id_user INT AUTO_INCREMENT UNIQUE UNSIGNED,
-  email VARCHAR(128) NOT NULL UNIQUE,
+  id_user INT AUTO_INCREMENT,
+  email VARCHAR(128) UNIQUE NOT NULL,
   password VARCHAR(64) NOT NULL,
-  registration_date DATETIME(64) NOT NULL,
+  registration_date DATETIME(6) NOT NULL,
   avatar_link varchar(256),
   PRIMARY KEY (`id_user`)
 );
 
 CREATE TABLE IF NOT EXISTS posts (
-  id_post INT AUTO_INCREMENT UNIQUE UNSIGNED,
-  post_date DATETIME(64) NOT NULL,
+  id_post INT UNSIGNED AUTO_INCREMENT,
+  post_date DATETIME(6) NOT NULL,
   title varchar(256),
   text_content TEXT,
   quote_author varchar(256) default '',
@@ -27,34 +25,34 @@ CREATE TABLE IF NOT EXISTS posts (
   PRIMARY KEY (`id_post`)
 );
 
-CREATE INDEX i_for_user_post ON posts(id_user);
+CREATE INDEX index_user_post ON posts(id_user);
 
 CREATE TABLE IF NOT EXISTS comments (
-  id_comment INT AUTO_INCREMENT UNIQUE UNSIGNED,
-  comment_date DATETIME(64) NOT NULL,
+  id_comment INT UNSIGNED AUTO_INCREMENT,
+  comment_date DATETIME(6) NOT NULL,
   comment_text TEXT NOT NULL default '',
-  id_user INT UNSIGNED,
-  id_post INT UNSIGNED,
+  id_user INT,
+  id_post INT,
   PRIMARY KEY (`id_comment`)
 );
 
-CREATE INDEX i_for_user_comment ON comments(id_user);
-CREATE INDEX i_for_post_comment ON comments(id_post);
+CREATE INDEX index_user_comment ON comments(id_user);
+CREATE INDEX index_post_comment ON comments(id_post);
 
 CREATE TABLE IF NOT EXISTS likes (
-  id_like INT AUTO_INCREMENT UNIQUE UNSIGNED,
-  id_user INT NOT NULL UNSIGNED,
-  id_post INT NOT NULL UNSIGNED,
+  id_like INT UNSIGNED AUTO_INCREMENT,
+  id_user INT NOT NULL,
+  id_post INT NOT NULL,
   PRIMARY KEY (`id_like`)
 );
 
-CREATE INDEX i_for_user_like ON comments(id_user);
-CREATE INDEX i_for_post_like ON comments(id_post);
+CREATE INDEX index_user_like ON comments(id_user);
+CREATE INDEX index_post_like ON comments(id_post);
 
 CREATE TABLE IF NOT EXISTS subscriptions (
-  id_subscription INT AUTO_INCREMENT UNIQUE UNSIGNED,
-  id_subscriber INT NOT NULL UNSIGNED,
-  id_receiver_sub INT NOT NULL UNSIGNED,
+  id_subscription INT UNSIGNED AUTO_INCREMENT,
+  id_subscriber INT NOT NULL,
+  id_receiver_sub INT NOT NULL,
   PRIMARY KEY (`id_subscription`)
 );
 
@@ -62,11 +60,11 @@ CREATE INDEX subscriber_index ON subscriptions(id_subscriber);
 CREATE INDEX receiver_sub_index ON subscriptions(id_receiver_sub);
 
 CREATE TABLE IF NOT EXISTS messages (
-  id_message INT AUTO_INCREMENT UNIQUE UNSIGNED,
-  message_date DATETIME(64) NOT NULL,
+  id_message INT UNSIGNED AUTO_INCREMENT,
+  message_date DATETIME(6) NOT NULL,
   message_text TEXT NOT NULL default '',
-  id_who_writed INT NOT NULL UNSIGNED,
-  id_for_who_writed INT NOT NULL UNSIGNED,
+  id_who_writed INT NOT NULL,
+  id_for_who_writed INT NOT NULL,
   PRIMARY KEY (`id_message`)
 );
 
@@ -74,13 +72,13 @@ CREATE INDEX sender_index ON messages(id_who_writed);
 CREATE INDEX receiver_index ON messages(id_for_who_writed);
 
 CREATE TABLE IF NOT EXISTS hashtags (
-  id_hashtag INT AUTO_INCREMENT UNIQUE UNSIGNED,
+  id_hashtag INT UNSIGNED AUTO_INCREMENT,
   hashtag_title varchar(256) NOT NULL,
   PRIMARY KEY (`id_hashtag`)
 );
 
 CREATE TABLE IF NOT EXISTS contentTypes (
-  id_type TINYINT AUTO_INCREMENT UNIQUE UNSIGNED,
+  id_type TINYINT UNSIGNED AUTO_INCREMENT,
   content_type_title CHAR(1) NOT NULL,
   content_class_type VARCHAR(64) NOT NULL,
   PRIMARY KEY (`id_type`)
